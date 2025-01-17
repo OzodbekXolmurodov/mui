7bc036c314d88fb7561ff2ca584415659c042f76
OzodbekXolmurodov <ozodbekxolmurodov08@gmail.com>
2025-01-17 20:32:50 +0500

    salom dunyo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import AccordionTsx from "./components/AccordionTsx";
+import Header from "./components/Header";
+import ProductCard from "./components/ProductCard";
+import TableMui from "./components/Table";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ProductCard />
+      <AccordionTsx />
+      <TableMui />
+    </>
+  );
+}
+
+export default App;
